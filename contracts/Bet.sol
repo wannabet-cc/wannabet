4ce71ab005517d60c975ee0da59d13c759704d69
ncale <nicholasbrodeur@outlook.com>
2024-05-31 12:26:29 -0500

    auto format solidity files

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Bet {
-
     address public immutable creator;
     address public immutable participant;
     address public immutable arbitrator;
@@ -16,7 +15,14 @@ contract Bet {
 
     IERC20 public immutable token;
 
-    constructor(address _creator, address _participant, address _arbitrator, uint256 _amount, string memory _message, address _token) {
+    constructor(
+        address _creator,
+        address _participant,
+        address _arbitrator,
+        uint256 _amount,
+        string memory _message,
+        address _token
+    ) {
         creator = _creator;
         participant = _participant;
         arbitrator = _arbitrator;
@@ -27,13 +33,19 @@ contract Bet {
 
     event BetAccepted();
     event BetDeclined();
-	event BetSettled(address indexed winner);
+    event BetSettled(address indexed winner);
 
     function acceptBet() public {
-        require(msg.sender == participant, "Must be the participant to accept bet");
+        require(
+            msg.sender == participant,
+            "Must be the participant to accept bet"
+        );
         require(!accepted, "Bet has already been accepted");
         require(!settled, "Bet has already been settled");
-        require(amount <= IERC20(token).allowance(msg.sender, address(this)), "Must give approval to send tokens");
+        require(
+            amount <= IERC20(token).allowance(msg.sender, address(this)),
+            "Must give approval to send tokens"
+        );
 
         // Transfer tokens to contract
         bool success = token.transferFrom(msg.sender, address(this), amount);
@@ -45,10 +57,13 @@ contract Bet {
     }
 
     function declineBet() public {
-        require(msg.sender == participant, "Must be the participant to decline bet");
+        require(
+            msg.sender == participant,
+            "Must be the participant to decline bet"
+        );
         require(!accepted, "Bet has already been accepted");
         require(!settled, "Bet has already been settled");
-        
+
         // Return tokens to original party
         bool success = token.transfer(creator, amount);
         require(success, "Token transfer failed");
@@ -59,10 +74,15 @@ contract Bet {
     }
 
     function settleBet(address _winner) public {
-		require(msg.sender == arbitrator, "Must be arbitrator to settle bet");
-		require(_winner == creator || _winner == participant || _winner == 0x0000000000000000000000000000000000000000, "Winner must be a betting party");
-		require(accepted, "Bet has not been accepted yet");
-		require(!settled, "Bet has already been settled");
+        require(msg.sender == arbitrator, "Must be arbitrator to settle bet");
+        require(
+            _winner == creator ||
+                _winner == participant ||
+                _winner == 0x0000000000000000000000000000000000000000,
+            "Winner must be a betting party"
+        );
+        require(accepted, "Bet has not been accepted yet");
+        require(!settled, "Bet has already been settled");
 
         // Transfer tokens to winner
         if (_winner == 0x0000000000000000000000000000000000000000) {
@@ -76,11 +96,11 @@ contract Bet {
             bool success = token.transfer(_winner, amount * 2);
             require(success, "Token transfer failed");
         }
-		
+
         // Update state variables
         winner = _winner;
-		settled = true;
+        settled = true;
         // Emit event
         emit BetSettled(_winner);
-	}
+    }
 }
