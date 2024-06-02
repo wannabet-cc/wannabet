fb24723b8cecb468cb31bf034500bc0d1a360e54
ncale <nicholasbrodeur@outlook.com>
2024-06-01 19:16:31 -0500

    make 'inRange' function inclusive

--- frame/src/screens/utils.ts
+++ frame/src/screens/utils.ts
@@ -3,8 +3,8 @@ function isInt(value: number) {
 }
 
 function isIntInRange(value: number, low: number, high: number) {
-  const isAboveLow = value > low;
-  const isBelowHigh = value < high;
+  const isAboveLow = value >= low;
+  const isBelowHigh = value <= high;
   return isInt(value) && isAboveLow && isBelowHigh;
 }
 
