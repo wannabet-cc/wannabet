b91e2d004f36132e3a253c5cca714e132b8f52ba
ncale <nicholasbrodeur@outlook.com>
2024-06-01 20:10:03 -0500

    change switch statement to if/else for better scope management

--- frame/src/screens/create.tsx
+++ frame/src/screens/create.tsx
@@ -25,103 +25,100 @@ export const createScreen = async (
     });
   }
 
-  switch (pageNum) {
-    case 1:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>Who are you betting with?</span>
-          </div>
-        ),
-        intents: [
-          <TextInput placeholder="e.g. example.eth or 0xabc..." />,
-          <Button action={`/home`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 2:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>Who would you like to arbitrate?</span>
-          </div>
-        ),
-        intents: [
-          <TextInput placeholder="e.g. example.eth or 0xabc..." />,
-          <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 3:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>How much USDC do you want to bet?</span>
-          </div>
-        ),
-        intents: [
-          <TextInput placeholder="e.g. 5" />,
-          <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 4:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>What are the terms?</span>
-          </div>
-        ),
-        intents: [
-          <TextInput placeholder="e.g. ETH price will be $5k by..." />,
-          <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 5:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>
-              Authorize the contract to move your wager to the bet contract
-            </span>
-          </div>
-        ),
-        intents: [
-          <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 6:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>Deploy your bet</span>
-          </div>
-        ),
-        intents: [
-          <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
-          <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
-        ],
-      });
-    case 7:
-      return c.res({
-        image: (
-          <div style={{ ...backgroundStyles }}>
-            <span style={{ color: "gray" }}>{pageNum}/7</span>
-            <span>Bet created! Share.</span>
-          </div>
-        ),
-        intents: [<Button action="/home" children={"Home"} />],
-      });
-    default:
-      null;
+  if (pageNum === 1) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>Who are you betting with?</span>
+        </div>
+      ),
+      intents: [
+        <TextInput placeholder="e.g. example.eth or 0xabc..." />,
+        <Button action={`/home`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 2) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>Who would you like to arbitrate?</span>
+        </div>
+      ),
+      intents: [
+        <TextInput placeholder="e.g. example.eth or 0xabc..." />,
+        <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 3) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>How much USDC do you want to bet?</span>
+        </div>
+      ),
+      intents: [
+        <TextInput placeholder="e.g. 5" />,
+        <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 4) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>What are the terms?</span>
+        </div>
+      ),
+      intents: [
+        <TextInput placeholder="e.g. ETH price will be $5k by..." />,
+        <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 5) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>
+            Authorize the contract to move your wager to the bet contract
+          </span>
+        </div>
+      ),
+      intents: [
+        <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 6) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>Deploy your bet</span>
+        </div>
+      ),
+      intents: [
+        <Button action={`/create/${pageNum - 1}`} children={"Back"} />,
+        <Button action={`/create/${pageNum + 1}`} children={"Continue"} />,
+      ],
+    });
+  } else if (pageNum === 7) {
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ color: "gray" }}>{pageNum}/7</span>
+          <span>Bet created! Share.</span>
+        </div>
+      ),
+      intents: [<Button action="/home" children={"Home"} />],
+    });
   }
 
   return c.res({
