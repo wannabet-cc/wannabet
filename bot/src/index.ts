d34189dc920240aba00e71962186ae4e0aab1d77
ncale <nicholasbrodeur@outlook.com>
2024-06-12 21:32:32 -0500

    Add skeleton for parsing what type of event was emitted

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -13,10 +13,33 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express Server");
 });
 
+const betCreatedSignature =
+  "0xeb61722110fd856b0d96d3312d86d62fcda6eee1eee2366d2c10e1d564d120e8";
+const betAcceptedSignature =
+  "0xdd6dae32994530eefb2d3b21473a19ec9f41d294a4fd6353b9b16d2d2c674b96";
+const betDeclinedSignature =
+  "0x815a1274b6d601b9c13c3a4ca7a73f7f180c6808c6b73b68360880ab923d979a";
+const betSettledSignature =
+  "0x1263c5e68e09cb9dfb7e7df0f53d955963a974e73d6ef177fadeb882cd9629ab";
+
 app.post("/webhooks", (req: Request, res: Response) => {
   const eventData = req.body as EventData;
   const logData = eventData.event.data.block.logs;
-  if (logData) console.log("Log received:", eventData);
+
+  logData.forEach((log) => {
+    const eventSignature = log.topics[0];
+    if (eventSignature === betCreatedSignature) {
+      // handle bet creation
+    } else if (eventSignature === betAcceptedSignature) {
+      // handle bet accepted
+    } else if (eventSignature === betDeclinedSignature) {
+      // handle bet declined
+    } else if (eventSignature === betSettledSignature) {
+      // handle bet settled
+    } else {
+      // handle error
+    }
+  });
 
   res.status(200).send("Received");
 });
