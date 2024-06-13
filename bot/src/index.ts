2a138af77dd53d7cbc9677bb11ecec25da8cd931
ncale <nicholasbrodeur@outlook.com>
2024-06-12 20:58:03 -0500

    Add route for posting webhook data & webhook data type

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -5,12 +5,59 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express Server");
 });
 
+app.post("/webhooks", (req: Request, res: Response) => {
+  const eventData = req.body as EventData;
+  const logData = eventData.event.data.block.logs;
+  if (logData) console.log("Log received:", eventData);
+
+  res.status(200).send("Received");
+});
+
+type EventData = {
+  webhookId: string;
+  id: string;
+  createdAt: string;
+  type: string;
+  event: {
+    data: {
+      block: {
+        number: number;
+        timestamp: number;
+        logs: Log[];
+      };
+    };
+    sequenceNumber: string;
+  };
+};
+type Log = {
+  data: string;
+  topics: string[];
+  index: number;
+  account: {
+    address: string;
+  };
+  transaction: {
+    hash: string;
+    nonce: number;
+    index: number;
+    from: {
+      address: string;
+    };
+    to: {
+      address: string;
+    };
+    value: string;
+  };
+};
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
