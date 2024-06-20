764d78f984e224342d72a416131e68216784e505
ncale <nicholasbrodeur@outlook.com>
2024-06-20 18:30:21 -0500

    Rebuild frame to require a refresh on start

--- frame/src/screens/bet.tsx
+++ frame/src/screens/bet.tsx
@@ -7,17 +7,13 @@ import {
   MAINNET_BET_FACTORY_CONTRACT_ADDRESS,
 } from "../contracts/addresses";
 import { betAbi } from "../contracts/betAbi";
-import {
-  capitalizeFirstLetter,
-  fetchUser,
-  getBetDetails,
-  getPreferredAlias,
-} from "../utils";
+import { fetchUser, getBetDetails, getPreferredAlias } from "../utils";
 import { FiatTokenProxyAbi } from "../contracts/usdcAbi";
 import { type CustomFrameContext } from "..";
 import { BetIdSchema } from "../zodSchemas";
 
 export const betScreen = async (c: CustomFrameContext<"/bet/:betId">) => {
+  // -> Validate url
   const { betId } = c.req.param();
   const { success, data: parsedBetId } = BetIdSchema.safeParse(Number(betId));
   if (!success) {
@@ -32,29 +28,32 @@ export const betScreen = async (c: CustomFrameContext<"/bet/:betId">) => {
     });
   }
 
+  // -> Fetch contract address
   const arbitrumClient = arbitrumClientFn(c);
-
-  const betCount = await arbitrumClient.readContract({
-    address: MAINNET_BET_FACTORY_CONTRACT_ADDRESS,
-    abi: betFactoryAbi,
-    functionName: "betCount",
-  });
-  if (parsedBetId > betCount) {
-    return c.res({
-      image: (
-        <div style={{ ...backgroundStyles }}>
-          <span>Bet doesn't exist yet</span>
-        </div>
-      ),
-      intents: [<Button action={`/home`} children={"Home"} />],
-    });
-  }
-
   const contractAddress = await arbitrumClient.readContract({
     address: MAINNET_BET_FACTORY_CONTRACT_ADDRESS,
     abi: betFactoryAbi,
     functionName: "betAddresses",
     args: [BigInt(betId)],
+  });
+
+  // -> Check if start screen
+  const { frameData, url } = c;
+  if (frameData) {
+    // -> Fetch bet details & status
+    const { creator, participant, arbitrator, amount } = await getBetDetails(
+      c,
+      contractAddress
+    );
+    const winner = await arbitrumClient.readContract({
+      address: contractAddress,
+      abi: betAbi,
+      functionName: "winner",
+    });
+    const status = await arbitrumClient.readContract({
+      address: contractAddress,
+      abi: betAbi,
+      functionName: "getStatus",
   });
   const contractBalance = await arbitrumClient.readContract({
     address: MAINNET_ARBITRUM_USDC_CONTRACT_ADDRESS,
@@ -62,47 +61,20 @@ export const betScreen = async (c: CustomFrameContext<"/bet/:betId">) => {
     functionName: "balanceOf",
     args: [contractAddress],
   });
-  const { creator, participant, amount, message, arbitrator } =
-    await getBetDetails(c, contractAddress);
-  const status = await arbitrumClient.readContract({
-    address: contractAddress,
-    abi: betAbi,
-    functionName: "getStatus",
-  });
-  const winner = await arbitrumClient.readContract({
-    address: contractAddress,
-    abi: betAbi,
-    functionName: "winner",
-  });
-
-  const [creatorAlias, participantAlias, winnerAlias] = await Promise.all([
-    getPreferredAlias(c, creator),
-    getPreferredAlias(c, participant),
-    getPreferredAlias(c, winner),
-  ]);
-
-  const { frameData, url } = c;
-
-  // -> get user addresses
-  const apiKey = c.env.NEYNAR_API_KEY;
-  const userData = await fetchUser(apiKey, frameData!.fid);
-  const userAddressList = userData.users[0].verified_addresses.eth_addresses;
-
-  // -> check if at least one address is involved in the bet
+    // -> Check if user is in bet
   let isCreator = false,
     isParticipant = false,
     isArbitrator = false;
+    const userData = await fetchUser(c.env.NEYNAR_API_KEY, frameData.fid);
+    const userAddressList = userData.users[0].verified_addresses.eth_addresses;
   userAddressList.forEach((address) => {
-    const lcAddress = address.toLowerCase();
-    if (lcAddress === creator.toLowerCase()) isCreator = true;
-    if (lcAddress === participant.toLowerCase()) isParticipant = true;
-    if (lcAddress === arbitrator.toLowerCase()) isArbitrator = true;
+      if (address.toLowerCase() === creator.toLowerCase()) isCreator = true;
+      if (address.toLowerCase() === participant.toLowerCase())
+        isParticipant = true;
+      if (address.toLowerCase() === arbitrator.toLowerCase())
+        isArbitrator = true;
   });
-
-  const isTie = winner !== creator && winner !== participant;
-
-  const convertedAmount = Number(amount) / 10 ** 6;
-
+    // -> Create conditional intents
   const participantButtons =
     isParticipant && status === "pending"
       ? [
@@ -131,70 +103,22 @@ export const betScreen = async (c: CustomFrameContext<"/bet/:betId">) => {
           />,
         ]
       : [];
+    // -> Re-format fetched data
+    const isTie =
+      status === "settled" && winner !== creator && winner !== participant;
 
+    const winnerAlias = await getPreferredAlias(c, winner);
+    // -> Return screen
   return c.res({
     image: (
       <div style={{ ...backgroundStyles }}>
-        <span>WannaBet #{betId}</span>
-        <span style={{ ...subTextStyles, marginTop: 20 }}>
-          <span style={{ textDecorationLine: "underline", marginRight: 12 }}>
-            {creatorAlias}
-          </span>
-          {" bet "}
-          <span
-            style={{
-              textDecorationLine: "underline",
-              marginLeft: 12,
-              marginRight: 12,
-            }}
-          >
-            {participantAlias}
-          </span>
-          {convertedAmount} USDC that:
-        </span>
-        <span
-          style={{
-            ...subTextStyles,
-            marginTop: 30,
-            marginBottom: 48,
-            color: "lightGreen",
-            padding: 8,
-            paddingLeft: 36,
-            borderLeft: "4px",
-          }}
-        >
-          {message}.
-        </span>
-        {status === "pending" && (
-          <span style={{ ...subTextStyles }}>
-            {capitalizeFirstLetter(status)}: {participantAlias} can accept or
-            decline
-          </span>
-        )}
-        {status === "expired" && (
-          <span style={{ ...subTextStyles }}>
-            {capitalizeFirstLetter(status)}: {participantAlias} didn't accept in
-            time. The bet creator can retrieve their funds.
-          </span>
-        )}
-        {status === "declined" && (
-          <span style={{ ...subTextStyles }}>
-            {capitalizeFirstLetter(status)}: {participantAlias} declined the bet
-          </span>
-        )}
-        {status === "accepted" && (
-          <span style={{ ...subTextStyles }}>
-            {capitalizeFirstLetter(status)}: Awaiting {arbitrator}&apos;s
-            decision
-          </span>
-        )}
+          <span style={{ ...subTextStyles }}>{`Bet #${betId}`}</span>
+          {status === "pending" && <span>Proposed, no response yet</span>}
+          {status === "accepted" && <span>Bet Accepted</span>}
+          {status === "declined" && <span>Bet Declined</span>}
+          {status === "expired" && <span>Bet Expired</span>}
         {status === "settled" && (
-          <span style={{ ...subTextStyles }}>
-            {capitalizeFirstLetter(status)}:{" "}
-            {isTie
-              ? "The bet tied and the pot was split."
-              : `${winnerAlias} won.`}
-          </span>
+            <span>{isTie ? "Tie!" : `${winnerAlias} won!`}</span>
         )}
       </div>
     ),
@@ -214,4 +138,32 @@ export const betScreen = async (c: CustomFrameContext<"/bet/:betId">) => {
     ],
     title: "WannaBet",
   });
+  } else {
+    // -> Return start screen
+    return c.res({
+      image: (
+        <div style={{ ...backgroundStyles }}>
+          <span style={{ ...subTextStyles }}>{`Bet #${betId}`}</span>
+          <span>Click to see status</span>
+        </div>
+      ),
+      intents: [
+        <Button
+          action={`/bet/${betId}`}
+          value="refresh"
+          children={"See Status"}
+        />,
+        <Button.Link
+          href={`https://arbiscan.io/address/${contractAddress}`}
+          children={"Arbiscan"}
+        />,
+        <Button
+          action={`${url}/create/1`}
+          value="create"
+          children={"Create new"}
+        />,
+      ],
+      title: "WannaBet",
+    });
+  }
 };
