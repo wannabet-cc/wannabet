d911612becc6fb2aaf8f75d4377d703cb3e3b509
ncale <nicholasbrodeur@outlook.com>
2024-06-26 17:03:16 -0500

    Fix: bot was missing mainnet chain in viem client

--- bot/src/viem.ts
+++ bot/src/viem.ts
@@ -1,9 +1,9 @@
 import { createPublicClient, http } from "viem";
-import { arbitrum } from "viem/chains";
+import { mainnet, arbitrum } from "viem/chains";
 import { MAINNET_ALCHEMY_URL, ARBITRUM_ALCHEMY_URL } from "./config";
 
 const mainnetClient = createPublicClient({
-  chain: arbitrum,
+  chain: mainnet,
   transport: http(MAINNET_ALCHEMY_URL),
 });
 const arbitrumClient = createPublicClient({
