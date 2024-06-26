f16313d3cd83b8a956ad73ca61c9ef343f740064
ncale <nicholasbrodeur@outlook.com>
2024-06-26 17:04:27 -0500

    Add viem clients

---
+++ website/src/services/viem.ts
@@ -0,0 +1,14 @@
+import { createPublicClient, http } from "viem";
+import { mainnet, arbitrum } from "viem/chains";
+import { MAINNET_ALCHEMY_URL, ARBITRUM_ALCHEMY_URL } from "../config";
+
+const mainnetClient = createPublicClient({
+  chain: mainnet,
+  transport: http(MAINNET_ALCHEMY_URL),
+});
+const arbitrumClient = createPublicClient({
+  chain: arbitrum,
+  transport: http(ARBITRUM_ALCHEMY_URL),
+});
+
+export { mainnetClient, arbitrumClient };
