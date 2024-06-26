55fbd00903808baec1c2eaad97ca291086e76ee4
ncale <nicholasbrodeur@outlook.com>
2024-06-26 14:18:17 -0500

    Add next js and shadcn ui

---
+++ website/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { BetListCard } from "@/components/bet-list-card";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <BetListCard />
+    </main>
+  );
+}
