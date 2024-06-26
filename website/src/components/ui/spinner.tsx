314faf8a5ed097f4d0208d131172f0ea6db2c4cb
ncale <nicholasbrodeur@outlook.com>
2024-06-26 17:57:17 -0500

    Change loading spinner to spin faster

--- website/src/components/ui/spinner.tsx
+++ website/src/components/ui/spinner.tsx
@@ -22,7 +22,7 @@ export const LoadingSpinner = ({
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={cn("animate-spin", className)}
+      className={cn("animate-spin-fast", className)}
     >
       <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
