d463ab15cdc0d6815894fc7ce2fcb09a669fd309
ncale <nicholasbrodeur@outlook.com>
2024-07-03 10:52:36 -0500

    Add a pause fn for testing

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -43,3 +43,7 @@ export function getAddressFromTokenName(tokenName: "USDC"): Address | string {
   if (tokenName === "USDC") return BASE_USDC_ADDRESS;
   else return "error";
 }
+
+export function pause(seconds: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
