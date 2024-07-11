c95f8d1fd26ae269334a107e37f9c3ad900c9203
ncale <nicholasbrodeur@outlook.com>
2024-07-11 17:13:30 -0500

    Add links to user tags

--- website/src/components/misc/user-badge.tsx
+++ website/src/components/misc/user-badge.tsx
@@ -1,17 +1,15 @@
 import { Badge } from "../ui/badge";
 
 export function UserBadge({ userAlias }: { userAlias: string }) {
-  return (
-    <Badge
-      variant={
-        userAlias.startsWith("@")
-          ? "user-farcaster"
-          : userAlias.endsWith(".eth")
-            ? "user-ens"
-            : "user-address"
-      }
-    >
-      {userAlias}
-    </Badge>
+  return userAlias.startsWith("@") ? (
+    <a href={`https://warpcast.com/${userAlias.slice(1)}`} target="_blank">
+      <Badge variant="user-farcaster">{userAlias}</Badge>
+    </a>
+  ) : userAlias.endsWith(".eth") ? (
+    <a href={`https://app.ens.domains/${userAlias}`} target="_blank">
+      <Badge variant="user-ens">{userAlias}</Badge>
+    </a>
+  ) : (
+    <Badge variant="user-address">{userAlias}</Badge>
   );
 }
