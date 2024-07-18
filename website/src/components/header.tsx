1e56a56aaeab922929d7e40543eaa4af4860311a
ncale <nicholasbrodeur@outlook.com>
2024-07-18 15:10:54 -0400

    Fix header so home link returns to the root rather than a fixed url

--- website/src/components/header.tsx
+++ website/src/components/header.tsx
@@ -6,7 +6,7 @@ import { CustomConnectButton } from "./rainbow/custom-connect-button";
 export function Header() {
   return (
     <header className="mb-12 flex w-full justify-between font-semibold">
-      <Link href="https://wannabet.cc" className="text-2xl lg:text-3xl">
+      <Link href="/" className="text-2xl lg:text-3xl">
         WannaBet 🤝
       </Link>
 
