b820fb6e6bfba7536d1d9556f3864d75047d1593
ncale <nicholasbrodeur@outlook.com>
2024-07-22 17:46:33 -0400

    Remove main-components.tsx

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,47 @@
-import { ExplorerComponent } from "@/components/main-components";
+import {
+  BetListCard,
+  MyBetList,
+  RecentBetList,
+} from "../components/bet-list-card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/tabs";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="w-full md:px-8">
       <ExplorerComponent />
     </main>
+  );
+}
+
+export function ExplorerComponent() {
+  return (
+    <Tabs defaultValue="recent" className="space-y-2">
+      <div className="flex justify-between">
+        <TabsList>
+          <TabsTrigger value="recent">Recent</TabsTrigger>
+          <TabsTrigger value="my">Mine</TabsTrigger>
+        </TabsList>
+        <Button variant="outline" asChild>
+          <Link href="/create">+ Create New</Link>
+        </Button>
+      </div>
+      <TabsContent value="recent">
+        <BetListCard title="Recent bets">
+          <RecentBetList />
+        </BetListCard>
+      </TabsContent>
+      <TabsContent value="my">
+        <BetListCard title="My bets">
+          <MyBetList />
+        </BetListCard>
+      </TabsContent>
+    </Tabs>
   );
 }
