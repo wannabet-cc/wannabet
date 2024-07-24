d712f2e4f6288a673dd8cce505ec150571253c94
ncale <nicholasbrodeur@outlook.com>
2024-07-24 09:20:57 -0400

    Delete "AccountCard" component

--- website/src/app/account/page.tsx
+++ website/src/app/account/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { ContactInformation } from "./contact-information";
 import { WalletList } from "./wallet-list";
 import { usePrivy } from "@privy-io/react-auth";
 import { useRouter } from "next/navigation";
 import ActionsSection from "./actions-section";
 import { Separator } from "@/components/ui/separator";
+import { BackButton } from "@/components/back-button";
+import { LogoutButton } from "@/components/auth/logout-button";
 
 export default function AccountPage() {
   const { user, ready, authenticated } = usePrivy();
@@ -16,24 +24,22 @@ export default function AccountPage() {
   if (!user) <></>;
 
   return (
-    <main className="flex w-full flex-col items-center md:px-8">
-      <AccountCard>
-        <ContactInformation />
-        <WalletList />
-        <Separator />
-        <ActionsSection />
-      </AccountCard>
+    <main className="flex w-full flex-col items-center">
+      <div className="w-full space-y-2 md:px-8">
+        <BackButton />
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Account Details</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            <ContactInformation />
+            <WalletList />
+          </CardContent>
+          <CardFooter>
+            <LogoutButton />
+          </CardFooter>
+        </Card>
+      </div>
     </main>
-  );
-}
-
-function AccountCard({ children }: { children: React.ReactNode }) {
-  return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle>Account Details</CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-6">{children}</CardContent>
-    </Card>
   );
 }
