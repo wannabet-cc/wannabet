376f210a488a3865fdff637e766581b53f1b1eb1
ncale <nicholasbrodeur@outlook.com>
2024-07-24 09:16:29 -0400

    Add link to account page when authenticated

--- website/src/components/auth/login-button.tsx
+++ website/src/components/auth/login-button.tsx
@@ -1,13 +1,26 @@
 import { usePrivy } from "@privy-io/react-auth";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 export function LoginButton() {
-  const { ready, authenticated, login } = usePrivy();
+  const { ready, authenticated, user, login } = usePrivy();
+
+  if (ready && authenticated) {
+    return (
+      <Button asChild variant="outline">
+        <Link href={"/account"}>{user?.id}</Link>
+      </Button>
+    );
+  }
+
   // Disable login when Privy is not ready or the user is already authenticated
   const disableLogin = !ready || (ready && authenticated);
-  return (
-    <Button disabled={disableLogin} onClick={login}>
-      Sign In
-    </Button>
-  );
+
+  if (!authenticated) {
+    return (
+      <Button disabled={disableLogin} onClick={login}>
+        Sign In
+      </Button>
+    );
+  }
 }
