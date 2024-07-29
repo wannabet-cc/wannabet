ef834f8c87724a537d6dbd9436345000a1c7177f
ncale <nicholasbrodeur@outlook.com>
2024-07-29 12:31:06 -0500

    Split the list of bets into its own reusable component

--- website/src/components/bet-lists.tsx
+++ website/src/components/bet-lists.tsx
@@ -1,107 +1,16 @@
 "use client";
 
-import {
-  type FormattedBets,
-  getRecentFormattedBets,
-  getUserFormattedBets,
-} from "@/services/services";
-import { type InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
-import { useAccount } from "wagmi";
-import { useRouter } from "next/navigation";
-import { LoginButton } from "./auth/login-button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "./ui/table";
-import { LoadingSpinner } from "./ui/spinner";
-import { ScrollArea } from "./ui/scroll-area";
-import { Button } from "./ui/button";
-import { UserBadge } from "./misc/user-badge";
-import { getTokenNameFromAddress } from "@/lib/utils";
+// Types
+import { type FormattedBets } from "@/services/services";
 
-function BetList({
-  data,
-  hasNextPage,
-  isFetchingNextPage,
-  fetchNextPage,
-}: {
-  data: InfiniteData<FormattedBets, unknown> | undefined;
-  hasNextPage: boolean;
-  isFetchingNextPage: boolean;
-  fetchNextPage: any;
-}) {
-  const router = useRouter();
-  return (
-    <ScrollArea className="h-80 w-full pr-2">
-      <Table>
-        <TableHeader className="sticky top-0 bg-card">
-          <TableRow>
-            <TableHead className="text-center">bet</TableHead>
-            <TableHead>amount</TableHead>
-            <TableHead>participants</TableHead>
-            <TableHead className="text-center">status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data?.pages.map((page) =>
-            page.items.map((bet, i) => (
-              <TableRow
-                key={i}
-                onClick={() => router.push(`/bet/${bet.betId}`)}
-                className="cursor-pointer"
-              >
-                <TableCell className="text-center">{bet.betId}</TableCell>
-                <TableCell>
-                  {bet.amount} {getTokenNameFromAddress(bet.token)}
-                </TableCell>
-                <TableCell>
-                  <UserBadge userAlias={bet.creatorAlias} />
-                  <span className="text-muted-foreground"> vs </span>
-                  <UserBadge userAlias={bet.participantAlias} />
-                </TableCell>
-                <TableCell className="text-center">
-                  {bet.status === "pending" ? (
-                    <span>⌛️</span>
-                  ) : bet.status === "accepted" ? (
-                    <span className="text-green-700">✓</span>
-                  ) : bet.status === "declined" || bet.status === "expired" ? (
-                    <span className="text-lg leading-none text-red-700">𐄂</span>
-                  ) : bet.status === "settled" ? (
-                    <span>💰</span>
-                  ) : (
-                    "..."
-                  )}
-                </TableCell>
-              </TableRow>
-            )),
-          )}
-          <TableRow>
-            <TableCell colSpan={4}>
-              <div className="mx-auto w-fit">
-                <Button
-                  variant="outline"
-                  size="sm"
-                  disabled={!hasNextPage || isFetchingNextPage}
-                  onClick={() => fetchNextPage()}
-                >
-                  {isFetchingNextPage
-                    ? "Loading..."
-                    : hasNextPage
-                      ? "Load More"
-                      : "Nothing more to load"}
-                </Button>
-              </div>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </ScrollArea>
-  );
-}
+// Hooks
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
+
+// Components
+import { LoginButton } from "./auth/login-button";
+import { LoadingSpinner } from "./ui/spinner";
+import { BetTable } from "./bet-table";
 
 export function RecentBetList() {
   const {
@@ -132,7 +41,7 @@ export function RecentBetList() {
   ) : status === "error" ? (
     "An error has occurred: " + { error }
   ) : (
-    <BetList
+    <BetTable
       data={data}
       hasNextPage={hasNextPage}
       isFetchingNextPage={isFetchingNextPage}
@@ -177,7 +86,7 @@ export function MyBetList() {
   ) : status === "error" ? (
     "An error has occurred: " + { error }
   ) : (
-    <BetList
+    <BetTable
       data={data}
       hasNextPage={hasNextPage}
       isFetchingNextPage={isFetchingNextPage}
---
+++ website/src/components/bet-table.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+// Types
+import { type FormattedBets } from "@/services/services";
+import { type InfiniteData } from "@tanstack/react-query";
+
+// Hooks
+import { useRouter } from "next/navigation";
+
+// Components
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { ScrollArea } from "./ui/scroll-area";
+import { Button } from "./ui/button";
+import { UserBadge } from "./misc/user-badge";
+
+// Utility Functions
+import { getTokenNameFromAddress } from "@/lib/utils";
+
+export function BetTable({
+  data,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: {
+  data: InfiniteData<FormattedBets, unknown> | undefined;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: any;
+}) {
+  const router = useRouter();
+  return (
+    <ScrollArea className="h-80 w-full pr-2">
+      <Table>
+        <TableHeader className="sticky top-0 bg-card">
+          <TableRow>
+            <TableHead className="text-center">bet</TableHead>
+            <TableHead>amount</TableHead>
+            <TableHead>participants</TableHead>
+            <TableHead className="text-center">status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data?.pages.map((page) =>
+            page.items.map((bet, i) => (
+              <TableRow
+                key={i}
+                onClick={() => router.push(`/bet/${bet.betId}`)}
+                className="cursor-pointer"
+              >
+                <TableCell className="text-center">{bet.betId}</TableCell>
+                <TableCell>
+                  {bet.amount} {getTokenNameFromAddress(bet.token)}
+                </TableCell>
+                <TableCell>
+                  <UserBadge userAlias={bet.creatorAlias} />
+                  <span className="text-muted-foreground"> vs </span>
+                  <UserBadge userAlias={bet.participantAlias} />
+                </TableCell>
+                <TableCell className="text-center">
+                  {bet.status === "pending" ? (
+                    <span>⌛️</span>
+                  ) : bet.status === "accepted" ? (
+                    <span className="text-green-700">✓</span>
+                  ) : bet.status === "declined" || bet.status === "expired" ? (
+                    <span className="text-lg leading-none text-red-700">𐄂</span>
+                  ) : bet.status === "settled" ? (
+                    <span>💰</span>
+                  ) : (
+                    "..."
+                  )}
+                </TableCell>
+              </TableRow>
+            )),
+          )}
+          <TableRow>
+            <TableCell colSpan={4}>
+              <div className="mx-auto w-fit">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  disabled={!hasNextPage || isFetchingNextPage}
+                  onClick={() => fetchNextPage()}
+                >
+                  {isFetchingNextPage
+                    ? "Loading..."
+                    : hasNextPage
+                      ? "Load More"
+                      : "Nothing more to load"}
+                </Button>
+              </div>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </ScrollArea>
+  );
+}
