e3fe651437cdf48ae45268296bdf63f74ee98113
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-05 10:08:41 -0500

    Make namestone a class

--- website/src/services/namestone.ts
+++ website/src/services/namestone.ts
@@ -1,99 +1,89 @@
 import { Address } from "viem";
 
-const NAMESTONE_URL = "https://namestone.xyz/api/public_v1/";
-const NAMESTONE_API_KEY = "";
+class NameStoneService {
+  readonly #BASE_URL = "https://namestone.xyz/api/public_v1/";
+  readonly #DOMAIN = "wannabet.eth";
+  #apiKey = "";
 
-/** General purpose function for sending a GET request to namestone.xyz api */
-async function getNamestone<T>(
-  path: "get-names" | "search-names",
-  queryParams: string,
-): Promise<T> {
-  const res = await fetch(
-    `${NAMESTONE_URL}${path}?domain=wannabet.eth&${queryParams}`,
-    {
+  constructor(apiKey: string) {
+    this.#apiKey = apiKey;
+  }
+
+  /** General purpose function for sending a GET request to namestone.xyz api */
+  async #getData<T>(path: "get-names" | "search-names", queryParams: string): Promise<T> {
+    const res = await fetch(`${this.#BASE_URL}${path}?domain=${this.#DOMAIN}&${queryParams}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: NAMESTONE_API_KEY,
+        Authorization: this.#apiKey,
       },
-      next: {
-        revalidate: 0,
+      next: { revalidate: 0 },
+    });
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+    return res.json() as Promise<T>;
+  }
+
+  /** General purpose function for sending a POST request to namestone.xyz api */
+  async #postData(path: "set-name" | "claim-name" | "revoke-name" | "set-domain", body: any) {
+    const res = await fetch(this.#BASE_URL + path, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: this.#apiKey,
       },
-    },
-  );
-  return (await res.json()) as T;
+      body: JSON.stringify(body),
+      next: { revalidate: 0 },
+    });
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+  }
+
+  /** Posts to the namestone.xyz "Set Name" route */
+  async setName(name: string, address: Address, avatar?: string) {
+    const body = {
+      domain: this.#DOMAIN,
+      name,
+      address,
+      text_records: avatar ? { avatar } : undefined,
+    };
+    this.#postData("set-name", body);
+  }
+
+  /** Posts to the namestone.xyz "Claim Name" route */
+  async claimName(name: string, address: Address, avatar?: string) {
+    const body = {
+      domain: this.#DOMAIN,
+      name,
+      address,
+      text_records: avatar ? { avatar } : undefined,
+    };
+    this.#postData("claim-name", body);
+  }
+
+  /** Gets a list of names */ // ! Looking into pagination
+  async getNames(limit: number): Promise<NameStoneResponse> {
+    const queryParams = `limit=${limit}`;
+    const data = await this.#getData<NameStoneResponse>("get-names", queryParams);
+    return data;
+  }
+
+  /** Gets a single name matching an address */
+  async getName(address: Address): Promise<NameStoneUser | null> {
+    const queryParams = `address=${address}&limit=${1}`;
+    const data = await this.#getData<NameStoneResponse>("get-names", queryParams);
+    return data[0] || null;
+  }
+
+  /** Searches names based on a string query */ // ! Looking into pagination
+  async searchName(query: string, limit: number): Promise<NameStoneResponse> {
+    const queryParams = `name=${query}&limit=${limit}`;
+    const data = await this.#getData<NameStoneResponse>("search-names", queryParams);
+    return data;
+  }
 }
 
-/** General purpose function for sending a POST request to namestone.xyz api */
-async function postNamestone(
-  path: "set-name" | "claim-name" | "revoke-name" | "set-domain",
-  body: any,
-) {
-  fetch(NAMESTONE_URL + path, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: NAMESTONE_API_KEY,
-    },
-    body: JSON.stringify(body),
-    next: {
-      revalidate: 0,
-    },
-  });
-}
+type NameStoneResponse = NameStoneUser[];
 
-/** Posts to the namestone.xyz "Set Name" route */
-function setName(name: string, address: Address, avatar?: string) {
-  const body = {
-    domain: "wannabet.eth",
-    name,
-    address,
-    text_records: { avatar },
-  };
-  postNamestone("set-name", body);
-}
-
-/** Posts to the namestone.xyz "Claim Name" route */
-function claimName(name: string, address: Address, avatar?: string) {
-  const body = {
-    domain: "wannabet.eth",
-    name,
-    address,
-    text_records: { avatar },
-  };
-  postNamestone("claim-name", body);
-}
-
-/** Gets a list of names */ // ! Looking into pagination
-async function getNames(limit: number): Promise<NamestoneResponse> {
-  const queryParams = `limit=${limit}`;
-  const data = await getNamestone<NamestoneResponse>("get-names", queryParams);
-  return data;
-}
-
-/** Gets a single name matching an address */
-async function getName(address: Address): Promise<NamestoneUser> {
-  const queryParams = `address=${address}&limit=${1}`;
-  const data = await getNamestone<NamestoneResponse>("get-names", queryParams);
-  return data[0];
-}
-
-/** Searches names based on a string query */ // ! Looking into pagination
-async function searchName(
-  query: string,
-  limit: number,
-): Promise<NamestoneResponse> {
-  const queryParams = `name=${query}&limit=${limit}`;
-  const data = await getNamestone<NamestoneResponse>(
-    "search-names",
-    queryParams,
-  );
-  return data;
-}
-
-type NamestoneResponse = NamestoneUser[];
-
-type NamestoneUser = {
+type NameStoneUser = {
   name: string;
   address: Address;
   domain: string; // ens name
@@ -101,3 +91,7 @@ type NamestoneUser = {
     avatar: string; // url
   };
 };
+
+const nameStoneService = new NameStoneService(process.env.NAMESTONE_API_KEY || "");
+
+export { nameStoneService, NameStoneService, type NameStoneUser, type NameStoneResponse };
