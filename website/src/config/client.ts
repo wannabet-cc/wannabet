96dfb5c3c6f38caea4504cc1f94d4d69f3aa982a
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-06 16:40:05 -0500

    Use different privy app ids for dev v prod environments

--- website/src/config/client.ts
+++ website/src/config/client.ts
@@ -1,6 +1,17 @@
+// Wallet Connect
 export const NEXT_PUBLIC_WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID || "";
+
+// Alchemy
 export const NEXT_PUBLIC_MAINNET_ALCHEMY_URL = process.env.NEXT_PUBLIC_MAINNET_ALCHEMY_URL || "";
 export const NEXT_PUBLIC_BASE_ALCHEMY_URL = process.env.NEXT_PUBLIC_BASE_ALCHEMY_URL || "";
 export const NEXT_PUBLIC_ARB_ALCHEMY_URL = process.env.NEXT_PUBLIC_ARB_ALCHEMY_URL || "";
+
+// Api
 export const NEXT_PUBLIC_BET_API_URL = process.env.NEXT_PUBLIC_BET_API_URL || "";
+
+// Neynar
 export const NEXT_PUBLIC_NEYNAR_API_KEY = process.env.NEXT_PUBLIC_NEYNAR_API_KEY || "";
+
+// Privy
+export const PRIVY_APP_ID =
+  process.env.NEXT_PUBLIC_PRIVY_APP_ID_DEV || process.env.NEXT_PUBLIC_PRIVY_APP_ID_PROD || "clyyp3crh0ar0yht8gj1gzj8t";
