f627c1c2c6a55e1b103d3d962bf749556e2cba89
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-08 18:56:06 -0500

    Add `ensureTokenApproval` to `wallet-functions.ts`

--- website/src/lib/wallet-functions.ts
+++ website/src/lib/wallet-functions.ts
@@ -5,12 +5,14 @@ import type { Address } from "viem";
 // Wagmi
 import { readContract, waitForTransactionReceipt, writeContract } from "@wagmi/core";
 import { config } from "@/app/providers";
-// Constants
+// Contract Imports
 import { FiatTokenProxyAbi } from "@/abis/FiatTokenProxyAbi";
+import { baseContracts } from "./contracts";
 
 /**
  * This file includes functions that are intended to be used on the CLIENT. Using
- * these on the server will result in an error.
+ * these on the server will result in an error due to importing the wagmi config
+ * from the providers file.
  */
 
 /**
@@ -22,8 +24,33 @@ export async function hasEnoughTokens(account: Address, token: Address, tokenThr
     address: token,
     abi: FiatTokenProxyAbi,
     functionName: "balanceOf",
-    args: [account!],
+    args: [account],
   });
   if (!balance) throw new Error("Failed to read users' token balance");
-  return balance > tokenThreshold;
+  return balance >= tokenThreshold;
+}
+
+/**
+ * Prompt user to approve token transfer to the "BetFactory" contract if tokens
+ * aren't already approved.
+ */
+export async function ensureTokenApproval(account: Address, token: Address, tokenAmount: bigint): Promise<void> {
+  const preexistingApprovedAmount = await readContract(config, {
+    address: token,
+    abi: FiatTokenProxyAbi,
+    functionName: "allowance",
+    args: [account, baseContracts.getAddressFromName("BetFactory")],
+  });
+  if (preexistingApprovedAmount < tokenAmount) {
+    const approveHash = await writeContract(config, {
+      address: token,
+      abi: FiatTokenProxyAbi,
+      functionName: "approve",
+      args: [baseContracts.getAddressFromName("BetFactory"), tokenAmount],
+    });
+    const { status: approveStatus } = await waitForTransactionReceipt(config, {
+      hash: approveHash,
+    });
+    if (approveStatus === "reverted") throw new Error("Token approval reverted");
+  }
 }
