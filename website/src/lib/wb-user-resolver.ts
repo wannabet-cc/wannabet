8354f615f84a8b0a7bd90061f8c09995f2f08ef5
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-15 16:16:36 -0500

    refactor: implement WannaBetUser class in UserResolver class

--- website/src/lib/wb-user-resolver.ts
+++ website/src/lib/wb-user-resolver.ts
@@ -1,19 +1,20 @@
-import type { WBUser } from "./types/user";
 import { nameStoneService } from "@/services/namestone";
 import { abbreviateHex } from "@/utils";
 import { fetchEns } from ".";
-import { ensSchema, type TEnsName } from "./types/ens";
-import { nameStone_NameSchema, TNameStoneName } from "./types/namestone";
+import { ensSchema } from "./types/ens";
+import { nameStone_NameSchema } from "./types/namestone";
 import { addressSchema } from "./types";
 import { z } from "zod";
+import { Address } from "viem";
+import { WannaBetUser } from "./wb-user";
 
 /**
  * This is class must be executed on the server side.
  */
 
 class UserResolver {
-  /** Validates and returns an object of type WBUser */
-  static async getUser(userAlias: string): Promise<WBUser | null> {
+  /** Validates and returns a WannaBetUser */
+  static async getUser(userAlias: string): Promise<WannaBetUser | null> {
     try {
       // NameStone
       const validated_nameStoneAlias = nameStone_NameSchema.safeParse(userAlias);
@@ -25,11 +26,12 @@ class UserResolver {
           throw new Error("Name not found");
         }
         // Return
-        return {
+        return new WannaBetUser({
+          type: "NameStone",
           name: res[0].name,
           address: res[0].address,
           avatar: res[0].text_records && res[0].text_records.avatar,
-        };
+        });
       }
 
       // ENS
@@ -42,21 +44,23 @@ class UserResolver {
           throw new Error("No matching record for ENS found");
         }
         // Return
-        return {
+        return new WannaBetUser({
+          type: "ENS",
           name: res.name,
           address: res.address,
           avatar: res.avatar || undefined,
-        };
+        });
       }
 
       // Address
       const validated_addressAlias = addressSchema.safeParse(userAlias);
       if (validated_addressAlias.success) {
         // Return
-        return {
+        return new WannaBetUser({
+          type: "Address",
           name: abbreviateHex(validated_addressAlias.data),
           address: validated_addressAlias.data,
-        };
+        });
       }
 
       // Fallback
