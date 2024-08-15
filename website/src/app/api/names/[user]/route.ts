4f011722150c99af134e60a49ae518c5ba785a41
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-15 18:51:17 -0500

    refactor: Update name api route to use UserResolver class

--- website/src/app/api/names/[user]/route.ts
+++ website/src/app/api/names/[user]/route.ts
@@ -1,41 +1,19 @@
 import { addressSchema } from "@/lib/types";
 import { nameStone_NameSchema } from "@/lib/types/namestone";
+import { UserResolver } from "@/lib/wb-user-resolver";
 import { nameStoneService } from "@/services/namestone";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 export async function GET(req: NextRequest, { params }: { params: { user: string } }) {
   try {
-    // validate name
-    const validatedName = nameStone_NameSchema.safeParse(params.user);
-    if (validatedName.success) {
-      // send to service
-      const res = await nameStoneService.searchName(validatedName.data, 1);
-      // 404 if no item found
-      if (res.length === 0 || res[0].name !== validatedName.data) {
-        return NextResponse.json({ message: "Name not found" }, { status: 404 });
-      }
-      // return
-      return NextResponse.json({ message: "Name fetched successfully", data: res[0] }, { status: 200 });
+    const user = await UserResolver.getPreferredUser(params.user);
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
-    // validate address
-    const validatedAddress = addressSchema.safeParse(params.user);
-    if (validatedAddress.success) {
-      // send to service
-      const res = await nameStoneService.getName(validatedAddress.data);
-      // 404 if no item found
-      if (!res) return NextResponse.json({ message: "Address not found" }, { status: 404 });
-      // return
-      return NextResponse.json({ message: "Name fetched successfully", data: res }, { status: 200 });
-    }
-
-    // throw if both are invalid
-    throw new z.ZodError([...(validatedName.error.errors || []), ...(validatedAddress.error.errors || [])]);
+    return NextResponse.json({ message: "User fetched successfully", data: user }, { status: 200 });
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return NextResponse.json({ error: "Validation failed", details: error.errors }, { status: 400 });
-    }
     NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
