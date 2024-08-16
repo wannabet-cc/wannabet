66f9438859628ed64fb3c428a6e4fd4bd3bc6be9
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-16 14:30:30 -0500

    refactor: improve namestone name schema strictness

--- website/src/lib/types/namestone.ts
+++ website/src/lib/types/namestone.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 
 export const nameStone_NameSchema = z
   .string()
+  .trim()
+  .toLowerCase()
   .min(1)
   .max(18)
-  .regex(/^[a-zA-Z0-9]+$/, "Only alphanumeric characters are allowed");
+  .regex(/^[a-z0-9]+$/, "Only alphanumeric characters are allowed");
 
 export type TNameStoneName = z.infer<typeof nameStone_NameSchema>;
